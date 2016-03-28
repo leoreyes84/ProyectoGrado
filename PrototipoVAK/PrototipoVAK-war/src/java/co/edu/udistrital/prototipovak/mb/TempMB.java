b7f84ecfb88a11a0f73a353fba9039892f89daf9
lreyes <lreyes@IngProyectos01.colfuturo.org>
2016-03-28 15:47:13 -0500

    LR - Versión inicial

---
+++ PrototipoVAK/PrototipoVAK-war/src/java/co/edu/udistrital/prototipovak/mb/TempMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udistrital.prototipovak.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author lreyes
+ */
+@ManagedBean
+@RequestScoped
+public class TempMB {
+
+    /**
+     * Creates a new instance of TempMB
+     */
+    public TempMB() {
+    }
+    
+}
